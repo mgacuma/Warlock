0f2d39c72169d691e84035e132abd32b28bff93e
M. Gacuma <60759812+mgacuma@users.noreply.github.com>
2023-12-01 03:00:08 -0800

    refactor server

--- app.js
+++ app.js
@@ -1,66 +1,59 @@
 import express from 'express'
-import { Server as hServer } from 'http'
-import { Server as sServer } from 'socket.io'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
-var players = {};
+const players = {}
 
 class GameServer {
     constructor(){
-        const __dirname = new URL('.', import.meta.url).pathname;
-        this.expressApp = express();
-        this.expressApp.use(express.static(__dirname + '/public'));
-        // this.expressApp.use("/public/", express.static(__dirname + '/public'));
-        this.expressApp.get('/', function (req, res) {
-            res.sendFile(__dirname + '/index.html');
-        });
-        this.httpServer = new hServer(this.expressApp)
+        this.app = express();
+        this.httpServer = createServer(this.app)
         this.httpServer.keepAliveTimeout = 120 * 1000
         this.httpServer.headersTimeout = 120 * 1000
-        this.io = new sServer(this.httpServer);
+        this.io = new Server(this.httpServer, {});
     }
 
-    init(){
-        this.io.on('connection', function (socket) {
-            console.log('a user connected');
-            // create a new player and add it to our players object
-            players[socket.id] = {
-              rotation: 0,
-              x: Math.floor(Math.random() * 700) + 50,
-              y: Math.floor(Math.random() * 500) + 50,
-              playerId: socket.id,
-              team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-            };
-            // send the players object to the new player
-            socket.emit('currentPlayers', players);
-            // update all other players of the new player
-            socket.broadcast.emit('newPlayer', players[socket.id]);
-            // when a player disconnects, remove them from our players object
-            socket.on('disconnect', function () {
-              console.log('user disconnected');
-              // remove this player from our players object
-              delete players[socket.id];
-              // emit a message to all players to remove this player
-              socket.broadcast.emit('playerdisconnect', socket.id);
-            });
+    setup(){
+      this.app.use(express.static('public'))
+      this.registerEndpoints()
+      this.registerHandlers()
+    }
+
+    registerEndpoints(){ 
+      this.app.get('/', function (req, res) {
+          console.log(__dirname)
+          res.sendFile(__dirname + '/index.html');
+      });
+    }
+
+    registerHandlers(){
+      this.io.on('connection', socket => {
+        console.log('Client joined lobby')
+        console.log(socket.id)
+
+        socket.join('lobby')
+
+        socket.on('disconnect', () => {
+          console.log('dc');
+        })
         
-            // when a player moves, update the player data
-            socket.on('playerMovement', function (movementData) {
-                players[socket.id].x = movementData.x;
-                players[socket.id].y = movementData.y;
-                players[socket.id].rotation = movementData.rotation;
-                // emit a message to all players about the player that moved
-                socket.broadcast.emit('playerMoved', players[socket.id]);
-            });
-          });
+        socket.on('createRoom', async () => {
+          socket.leave('lobby')
+          console.log('createRoom')
+          console.log(await this.io.fetchSockets())
+        })
+
+      });
     }
 
     run(){
-        this.httpServer.listen(8081, function () {
-            console.log(`Listening on `);
-          });
+      this.httpServer.listen(8081, function () {
+        console.log(`Listening on port 8080 `);
+      });
+
     }
 }
 
 const server = new GameServer()
-server.init()
+server.setup()
 server.run()
--- public/js/game.js
+++ public/js/game.js
@@ -1,28 +1,96 @@
+var socket
+const roundHalf = num => Math.round(num * 2) / 2
 
+/**
+ * The base resolution of this game is 360x640 @4.
+ * Means you should draw all your assets in a resolution of 1440x2560 or 2560x1440
+ * and pack and scale them down with a TexturePacker like https://www.codeandweb.com/texturepacker
+ */
+
+// is the user not happy with 'best',
+// simply set the graphics to 'medium'
+// (maybe store the settings in LocalStorage)
+const graphicsSettings = { best: 1, medium: 0.75, low: 0.5 }
+const DPR = window.devicePixelRatio * graphicsSettings.best
+const { width, height } = window.screen
+
+// Set width and height.
+const WIDTH = Math.round(Math.max(width, height) * DPR)
+const HEIGHT = Math.round(Math.min(width, height) * DPR)
+
+// Need a fixed screen ratio for your game
+// because it is to hard to make it responsive?
+// Simply lock the screen ratio by adapting the code as follow.
+// (On a desktop PC it should show all assets in @4)
+// const isMobile = () => Math.min(screen.width, screen.height) <= 480
+// const WIDTH = 640 * (isMobile() ? DPR : 4)
+// const HEIGHT = 360 * (isMobile() ? DPR : 4)
+
+// will be 1, 1.5, 2, 2.5, 3, 3.5 or 4
+export const assetsDPR = roundHalf(Math.min(Math.max(HEIGHT / 360, 1), 4))
+
+console.log('DPR = ', DPR)
+console.log('assetsDPR = ', assetsDPR)
+console.log('WIDTH = ', WIDTH)
+console.log('HEIGHT = ', HEIGHT)
+
+class SID extends Phaser.Scene {
+  constructor(){
+    super({key: 'SID', active: true})
+  }
+
+  create(){
+    this.sid = this.add.text(24,24, 'sid: ' + socket.id, {color: '#000', fontSize: 32})
+  }
+
+  update(){
+    this.sid.setText('sid: ' + socket.id)
+  }
+}
+
+class JoinRoom extends Phaser.Scene {
+  constructor(){
+    super({key: 'JoinRoom'})
+  }
+
+  create(){
+    const window = this.add.rectangle(300,300,300,300,'#f2f2f2')
+  }
+
+}
 
 class TitleScene extends Phaser.Scene {
   constructor(){
       super('TitleScene')
-      console.log('title made')
   }
 
+  preload(){}
+
   create(){
-      this.add.image(400, 300, 'sky');
-      this.add.text(20,20, 'loading gamsu...')
-      this.input.on('pointerup', function (pointer) {
+      this.add.text(500,520, 'Title Screen', {color: '#000', fontSize: 78})
+      const create = this.add.text(500,598, 'Create Room', {color: '#000', fontSize: 78})
+      create.setInteractive()
+      create.on('pointerup', () => {
+        console.log('emiting')
+        socket.emit('createRoom')
+      })
+      
+      const join = this.add.text(500,676, 'Join Room', {color: '#000', fontSize: 78})
+      join.setInteractive()
+      join.on('pointerup', () => {
+        this.scene.start('JoinRoom')
+      })
+  }
 
-        this.scene.start('GameScene');
+  update(){
 
-    }, this);
   }
 }
 
 class GameScene extends Phaser.Scene {
   constructor(){
       super('GameScene')
-      console.log('game made')
   }
-
 
   preload() {
       this.load.image('ship', 'assets/spaceShips_001.png');
@@ -30,26 +98,26 @@ class GameScene extends Phaser.Scene {
     }
   
   create() {
-    this.add.text(20,20,'gaming...')
+    console.log('create called')
+    this.add.text(290,290,'gaming...', {fontSize: 86})
       var self = this;
-      this.socket = io();
       this.otherPlayers = this.physics.add.group();
   
-      this.socket.on('currentPlayers', function (players) {
+      socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
           if (players[id].playerId === self.socket.id) {
-            addPlayer(self, players[id]);
+            self.addPlayer(self, players[id]);
           } else {
-            addOtherPlayers(self, players[id]);
+            self.addOtherPlayers(self, players[id]);
           }
         });
       });
   
-      this.socket.on('newPlayer', function (playerInfo) {
-        addOtherPlayers(self, playerInfo);
+      socket.on('newPlayer', function (playerInfo) {
+        self.addOtherPlayers(self, playerInfo);
       });
   
-      this.socket.on('playerdisconnect', function (playerId) {
+      socket.on('playerdisconnect', function (playerId) {
         self.otherPlayers.getChildren().forEach(function (otherPlayer) {
           if (playerId === otherPlayer.playerId) {
             otherPlayer.destroy();
@@ -57,7 +125,7 @@ class GameScene extends Phaser.Scene {
         });
       });
   
-      this.socket.on('playerMoved', function (playerInfo) {
+      socket.on('playerMoved', function (playerInfo) {
           self.otherPlayers.getChildren().forEach(function (otherPlayer) {
             if (playerInfo.playerId === otherPlayer.playerId) {
               otherPlayer.setRotation(playerInfo.rotation);
@@ -70,36 +138,36 @@ class GameScene extends Phaser.Scene {
     }
   
   update() {
-      if (this.ship) {
-          // emit player movement
-          var x = this.ship.x;
-          var y = this.ship.y;
-          var r = this.ship.rotation;
-          if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-            this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
-          }
-          // save old position data
-          this.ship.oldPosition = {
-            x: this.ship.x,
-            y: this.ship.y,
-            rotation: this.ship.rotation
-          };
-          if (this.cursors.left.isDown) {
-            this.ship.setAngularVelocity(-150);
-          } else if (this.cursors.right.isDown) {
-            this.ship.setAngularVelocity(150);
-          } else {
-            this.ship.setAngularVelocity(0);
-          }
-        
-          if (this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-          } else {
-            this.ship.setAcceleration(0);
-          }
-          // this.physics.world.wrap(this.ship, 5);
+    if (this.ship) {
+        // emit player movement
+        var x = this.ship.x;
+        var y = this.ship.y;
+        var r = this.ship.rotation;
+        if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+          this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
         }
+        // save old position data
+        this.ship.oldPosition = {
+          x: this.ship.x,
+          y: this.ship.y,
+          rotation: this.ship.rotation
+        };
+        if (this.cursors.left.isDown) {
+          this.ship.setAngularVelocity(-150);
+        } else if (this.cursors.right.isDown) {
+          this.ship.setAngularVelocity(150);
+        } else {
+          this.ship.setAngularVelocity(0);
+        }
+      
+        if (this.cursors.up.isDown) {
+          this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+        } else {
+          this.ship.setAcceleration(0);
+        }
+        // this.physics.world.wrap(this.ship, 5);
     }
+  }
   
   addPlayer(self, playerInfo) {
       self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
@@ -126,11 +194,16 @@ class GameScene extends Phaser.Scene {
     }
 }
 
-var config = {
+const config = {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 1080,
-  height: 780,
+  backgroundColor: '#ffffff',
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: WIDTH,
+    height: HEIGHT
+  },
   physics: {
     default: 'arcade',
     arcade: {
@@ -138,8 +211,12 @@ var config = {
       gravity: { y: 0 }
     }
   },
-  scene: [TitleScene, GameScene]
+  scene: [TitleScene, GameScene, SID, JoinRoom]
 };
 
 
-var game = new Phaser.Game(config);
+window.addEventListener('load', () => {
+  socket = io()
+  socket.on('connect', () => { console.log(socket.id) })
+  new Phaser.Game(config)
+})
