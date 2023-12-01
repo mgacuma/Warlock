edce82072510601ece712cadb18ac9443d9f4bfb
M. Gacuma <miguel_gacuma@yahoo.com>
2023-11-30 16:23:04 -0800

    init

--- app.js
+++ app.js
@@ -1,61 +1,65 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+import express from 'express'
+import { Server as hServer } from 'http'
+import { Server as sServer } from 'socket.io'
 
-app.get("/", (req, res) => res.type('html').send(html));
+var players = {};
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+class GameServer {
+    constructor(){
+        const __dirname = new URL('.', import.meta.url).pathname;
+        this.expressApp = express();
+        this.expressApp.use(express.static(__dirname + '/public'));
+        this.expressApp.get('/', function (req, res) {
+            res.sendFile(__dirname + '/index.html');
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+        this.httpServer = new hServer(this.expressApp)
+        this.httpServer.keepAliveTimeout = 120 * 1000
+        this.httpServer.headersTimeout = 120 * 1000
+        this.io = new sServer(this.httpServer);
+    }
+
+    init(){
+        this.io.on('connection', function (socket) {
+            console.log('a user connected');
+            // create a new player and add it to our players object
+            players[socket.id] = {
+              rotation: 0,
+              x: Math.floor(Math.random() * 700) + 50,
+              y: Math.floor(Math.random() * 500) + 50,
+              playerId: socket.id,
+              team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+            };
+            // send the players object to the new player
+            socket.emit('currentPlayers', players);
+            // update all other players of the new player
+            socket.broadcast.emit('newPlayer', players[socket.id]);
+            // when a player disconnects, remove them from our players object
+            socket.on('disconnect', function () {
+              console.log('user disconnected');
+              // remove this player from our players object
+              delete players[socket.id];
+              // emit a message to all players to remove this player
+              socket.broadcast.emit('playerdisconnect', socket.id);
+            });
+        
+            // when a player moves, update the player data
+            socket.on('playerMovement', function (movementData) {
+                players[socket.id].x = movementData.x;
+                players[socket.id].y = movementData.y;
+                players[socket.id].rotation = movementData.rotation;
+                // emit a message to all players about the player that moved
+                socket.broadcast.emit('playerMoved', players[socket.id]);
+            });
+          });
+    }
+
+    run(){
+        this.httpServer.listen(8081, function () {
+            console.log(`Listening on `);
+          });
+    }
+}
+
+const server = new GameServer()
+server.init()
+server.run()
